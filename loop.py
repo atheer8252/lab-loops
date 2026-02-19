5979b12bd4d823cda77057cb8e2b5d39924b4164
Atheer Alshahrani <utherr120@gmail.com>
2026-02-19 09:54:13 +0300

    add loop

--- loop.py
+++ loop.py
@@ -1,1 +1,10 @@
-﻿print('This is my loop file')
+﻿while True:
+    user_input = input("Please enter a number: ")
+
+    # Check if the input is a number
+    if user_input.isdigit():
+        number = int(user_input)
+        print("Valid input:", number)
+        break  # Exit the loop if the input is valid
+    else:
+        print("Invalid input, please try again.")
